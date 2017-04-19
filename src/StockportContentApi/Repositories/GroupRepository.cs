bf44a227152b3d9abbad7de06d984fbe496d1e1a
Gary Holland <gary.holland@stockport.gov.uk>
2017-04-19 09:11:11 +0100

    Gary/Raj fixed unit tests

--- src/StockportContentApi/Repositories/GroupRepository.cs
+++ src/StockportContentApi/Repositories/GroupRepository.cs
@@ -53,14 +53,14 @@ namespace StockportContentApi.Repositories
                     .Include(1)
                     .Limit(ContentfulQueryValues.LIMIT_MAX);
 
-            //if (lat != 53.40581278523235 && lon != -2.158041000366211)
-            //{
-            //    builder = builder.FieldEquals("fields.mapPosition[near]", lat + "," + lon + ",3.2");
-            //}
-            //else
-            //{
-            //    builder = builder.FieldEquals("fields.mapPosition[near]", lat + "," + lon + ",10");
-            //}
+            if (lat != 53.40581278523235 && lon != -2.158041000366211)
+            {
+                builder = builder.FieldEquals("fields.mapPosition[near]", lat + "," + lon + ",3.2");
+            }
+            else
+            {
+                builder = builder.FieldEquals("fields.mapPosition[near]", lat + "," + lon + ",10");
+            }
 
             var entries = await _client.GetEntriesAsync(builder);
             if (entries == null) return HttpResponse.Failure(HttpStatusCode.NotFound, "No groups found");
