a786cea002863c188b4b34c58c9f0882d9212fe1
Raj Paili <raj.paili@stockport.gov.uk>
2017-06-19 12:19:51 +0100

    #990 Dave/Raj added unit test ExpandingLinkBoxContentfulfactoryTest

---
+++ test/StockportContentApiTests/Unit/ContentfulFactories/ExpandingLinkBoxContentfulfactoryTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Moq;
+using StockportContentApi.ContentfulFactories;
+using StockportContentApi.ContentfulModels;
+using StockportContentApi.Model;
+using StockportContentApi.Utils;
+using StockportContentApiTests.Unit.Builders;
+using System;
+using Xunit;
+
+namespace StockportContentApiTests.Unit.ContentfulFactories
+{
+    public class ExpandingLinkBoxContentfulfactoryTest
+    {
+        private readonly Mock<ITimeProvider> _timeProvider = new Mock<ITimeProvider>();
+
+        public ExpandingLinkBoxContentfulfactoryTest()
+        {
+            _timeProvider.Setup(o => o.Now()).Returns(new DateTime(2017, 02, 02));
+        }
+
+        [Fact]
+        public void ShouldCreateAnExpandingLinkBoxFromAContentfulExpandingLinkBox()
+        {
+            var contentfulExpandingLinkBox = new ContentfulExpandingLinkBoxBuilder().Build();
+            var subItemFactory = new SubItemContentfulFactory(_timeProvider.Object);
+            var factory = new ExpandingLinkBoxContentfulfactory(subItemFactory);
+           
+            var expandingLinkBox = factory.ToModel(contentfulExpandingLinkBox);
+          
+            expandingLinkBox.Title.Should().Be(contentfulExpandingLinkBox.Title);
+            expandingLinkBox.Links[0].Slug.Should().Be(contentfulExpandingLinkBox.Links[0].Slug);
+        }
+    }
+}
