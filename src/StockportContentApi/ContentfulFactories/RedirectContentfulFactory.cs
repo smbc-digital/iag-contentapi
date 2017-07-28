710f2350ac0b93a09d95c168ef70eeadac08c58b
Raj Paili <raj.paili@stockport.gov.uk>
2017-07-28 16:17:40 +0100

    Raj #893 Redirect uses new sdk

---
+++ src/StockportContentApi/ContentfulFactories/RedirectContentfulFactory.cs
@@ -0,0 +1,14 @@
+﻿using Contentful.Core.Models;
+using StockportContentApi.ContentfulModels;
+using StockportContentApi.Model;
+
+namespace StockportContentApi.ContentfulFactories
+{
+    public class RedirectContentfulFactory : IContentfulFactory<ContentfulRedirect, BusinessIdToRedirects>
+    {
+        public BusinessIdToRedirects ToModel(ContentfulRedirect entry)
+        {
+            return new BusinessIdToRedirects(entry.Redirects, entry.LegacyUrls);
+        }
+    }
+}
