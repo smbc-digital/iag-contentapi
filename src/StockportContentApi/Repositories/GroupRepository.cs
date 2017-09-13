5e5ac22a89d8469e1cb9e4c126652e0d16bf8a5a
Neil Redfern <neil.redfern@stockport.gov.uk>
2017-09-13 11:53:14 +0100

    Neil - changed to any lookup from null lookup

--- src/StockportContentApi/Repositories/GroupRepository.cs
+++ src/StockportContentApi/Repositories/GroupRepository.cs
@@ -117,7 +117,7 @@ namespace StockportContentApi.Repositories
             var group = _groupFactory.ToModel(entry);
             group.SetEvents(await _eventRepository.GetLinkedEvents<Group>(slug));           
 
-            if(group.CategoriesReference != null && group.SubCategories != null)
+            if(group.CategoriesReference.Any() && group.SubCategories.Any())
             {
                 group.SetLinkedGroups(await GetLinkedGroups(group.CategoriesReference, group.SubCategories));
             }            
