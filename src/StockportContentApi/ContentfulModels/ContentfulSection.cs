68cc3db5faf3bcf0ce76d4f1340542c72de437ab
Gary Holland <gary.holland@stockport.gov.uk>
2017-11-01 09:04:02 +0000

    Gary/Lukasz fix articles

--- src/StockportContentApi/ContentfulModels/ContentfulSection.cs
+++ src/StockportContentApi/ContentfulModels/ContentfulSection.cs
@@ -4,16 +4,11 @@ using Contentful.Core.Models;
 
 namespace StockportContentApi.ContentfulModels
 {
-    public class ContentfulSection : ContentfulReference, IContentfulModel
+    public class ContentfulSection : ContentfulReference
     {
-        public string Title { get; set; } = string.Empty;
-        public string Slug { get; set; } = string.Empty;
         public string Body { get; set; } = string.Empty;
         public List<ContentfulProfile> Profiles { get; set; } = new List<ContentfulProfile>();
         public List<Asset> Documents { get; set; } = new List<Asset>();
-        public DateTime SunriseDate { get; set; } = DateTime.MinValue.ToUniversalTime();
-        public DateTime SunsetDate { get; set; } = DateTime.MaxValue.ToUniversalTime();
         public List<ContentfulAlert> AlertsInline { get; set; } = new List<ContentfulAlert>();
-        public SystemProperties Sys { get; set; } = new SystemProperties();
     }
 }
