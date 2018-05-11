f4cc32f7e0ed975e256b76024296f51b0eb87d00
Lydia Gray <lydia.gray@stockport.gov.uk>
2018-05-11 15:45:13 +0100

    Lydia #924 - Updated content api for new privacy notice structure

--- src/StockportContentApi/Model/PrivacyNotice.cs
+++ src/StockportContentApi/Model/PrivacyNotice.cs
@@ -9,38 +9,34 @@ namespace StockportContentApi.Model
     {
         public string Slug { get; set; }
         public string Title { get; set; }
-        public string Directorate { get; set; }
-        public string ActivitiesAsset { get; set; }
-        public string TransactionsActivity { get; set; }
+        public string Category { get; set; }
+        public bool OutsideEu { get; set; }
+        public bool AutomatedDecision { get; set; }
         public string Purpose { get; set; }
         public string TypeOfData { get; set; }
         public string Legislation { get; set; }
         public string Obtained { get; set; }
         public string ExternallyShared { get; set; }
         public string RetentionPeriod { get; set; }
-        public string Conditions { get; set; }
-        public string ConditionsSpecial { get; set; }
         public string UrlOne { get; set; }
         public string UrlTwo { get; set; }
         public string UrlThree { get; set; }
 
         public PrivacyNotice() { }
 
-        public PrivacyNotice(string slug, string title, string directorate, string activitiesAsset, string transactionsActivity, string purpose, string typeOfData, string legislation, string obtained, string externallyShared, string retentionPeriod, string conditions, string conditionsSpecial, string urlOne, string urlTwo, string urlThree)
+        public PrivacyNotice(string slug, string title, string category, bool outsideEu, bool automatedDecision, string purpose, string typeOfData, string legislation, string obtained, string externallyShared, string retentionPeriod, string urlOne, string urlTwo, string urlThree)
         {
             Slug = slug;
             Title = title;
-            Directorate = directorate;
-            ActivitiesAsset = activitiesAsset;
-            TransactionsActivity = transactionsActivity;
+            Category = category;
+            OutsideEu = outsideEu;
+            AutomatedDecision = automatedDecision;
             Purpose = purpose;
             TypeOfData = typeOfData;
             Legislation = legislation;
             Obtained = obtained;
             ExternallyShared = externallyShared;
             RetentionPeriod = retentionPeriod;
-            Conditions = conditions;
-            ConditionsSpecial = conditionsSpecial;
             UrlOne = urlOne;
             UrlTwo = urlTwo;
             UrlThree = urlThree;
