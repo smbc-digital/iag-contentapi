f854376f4cf28260249c744c6d6915c4bcb458e7
Lydia Gray <lydia.gray@stockport.gov.uk>
2019-01-30 12:28:01 +0000

    refactor(Showcase): Refactored showcase models and factory

--- src/StockportContentApi/ContentfulFactories/ShowcaseContentfulFactory.cs
+++ src/StockportContentApi/ContentfulFactories/ShowcaseContentfulFactory.cs
@@ -3,7 +3,6 @@ using StockportContentApi.ContentfulModels;
 using StockportContentApi.Model;
 using StockportContentApi.Utils;
 using Microsoft.AspNetCore.Http;
-using StockportContentApi.Services.Profile;
 
 
 namespace StockportContentApi.ContentfulFactories
@@ -25,7 +24,7 @@ namespace StockportContentApi.ContentfulFactories
 
         public ShowcaseContentfulFactory(IContentfulFactory<ContentfulReference, SubItem> subitemFactory, IContentfulFactory<ContentfulReference, Crumb> crumbFactory, ITimeProvider timeProvider, IContentfulFactory<ContentfulConsultation, Consultation> consultationFactory, IContentfulFactory<ContentfulSocialMediaLink, SocialMediaLink> socialMediaFactory, IContentfulFactory<ContentfulAlert, Alert> alertFactory, IContentfulFactory<ContentfulKeyFact, KeyFact> keyFactFactory, IContentfulFactory<ContentfulProfile, Profile> profileFactory,
             IContentfulFactory<ContentfulInformationList, InformationList> informationListFactory,
-            IHttpContextAccessor httpContextAccessor, 
+            IHttpContextAccessor httpContextAccessor,
             IContentfulFactory<ContentfulCallToActionBanner, CallToActionBanner> callToActionBannerContentfulFactory,
             IContentfulFactory<ContentfulVideo, Video> videoFactory)
         {
@@ -45,167 +44,104 @@ namespace StockportContentApi.ContentfulFactories
 
         public Showcase ToModel(ContentfulShowcase entry)
         {
-
-            var subItems = entry.SubItems.Where(subItem => ContentfulHelpers.EntryIsNotALink(subItem.Sys)
-                                                          && _dateComparer.DateNowIsWithinSunriseAndSunsetDates(subItem.SunriseDate, subItem.SunsetDate))
-                 .Select(subItem => _subitemFactory.ToModel(subItem)).ToList();
-
-            var tertiaryItems = entry.TertiaryItems.Where(subItem => ContentfulHelpers.EntryIsNotALink(subItem.Sys)
-                                                                     && _dateComparer.DateNowIsWithinSunriseAndSunsetDates(subItem.SunriseDate, subItem.SunsetDate))
-                .Select(subItem => _subitemFactory.ToModel(subItem)).ToList();
-
-            var title = !string.IsNullOrEmpty(entry.Title)
-                ? entry.Title
-                : "";
-
-            var slug = !string.IsNullOrEmpty(entry.Slug)
-                ? entry.Slug
-                : "";
-
             var heroImage = ContentfulHelpers.EntryIsNotALink(entry.HeroImage.SystemProperties)
                 ? entry.HeroImage.File.Url
                 : string.Empty;
-
-            var teaser = !string.IsNullOrEmpty(entry.Teaser)
-                ? entry.Teaser
-                : "";
-
-            var subHeading = !string.IsNullOrEmpty(entry.Subheading)
-                ? entry.Subheading
-                : "";
-
-            var eventSubheading = !string.IsNullOrEmpty(entry.EventSubheading)
-                ? entry.EventSubheading
-                : "";
-
-            var eventCategory = !string.IsNullOrEmpty(entry.EventCategory)
-                ? entry.EventCategory
-                : "";
-
-            var newsSubheading = !string.IsNullOrEmpty(entry.NewsSubheading)
-                ? entry.NewsSubheading
-                : "";
-
-            var newsCategoryTag = !string.IsNullOrEmpty(entry.NewsCategoryTag)
-                ? entry.NewsCategoryTag
-                : "";
-
-            var bodySubheading = !string.IsNullOrEmpty(entry.BodySubheading)
-                ? entry.BodySubheading
-                : "";
-
-            var body = !string.IsNullOrEmpty(entry.Body)
-                ? entry.Body
-                : "";
-
-            var emailAlertsTopicId = !string.IsNullOrEmpty(entry.EmailAlertsTopicId)
-                ? entry.EmailAlertsTopicId
-                : "";
-
-            var emailAlertsText = !string.IsNullOrEmpty(entry.EmailAlertsText)
-                ? entry.EmailAlertsText
-                : "";
-
-
-            var secondaryItems =
-                entry.SecondaryItems.Where(subItem => ContentfulHelpers.EntryIsNotALink(subItem.Sys)
-                && _dateComparer.DateNowIsWithinSunriseAndSunsetDates(subItem.SunriseDate, subItem.SunsetDate))
-                .Select(item => _subitemFactory.ToModel(item)).ToList();
 
             var primaryItems =
                 entry.PrimaryItems.Where(primItem => ContentfulHelpers.EntryIsNotALink(primItem.Sys)
                                                      && _dateComparer.DateNowIsWithinSunriseAndSunsetDates(primItem.SunriseDate, primItem.SunsetDate))
                     .Select(item => _subitemFactory.ToModel(item)).ToList();
 
-            var featuredItemSubheading = !string.IsNullOrEmpty(entry.FeaturedItemsSubheading)
-                ? entry.FeaturedItemsSubheading
-                : "";
+            var secondaryItems =
+                entry.SecondaryItems.Where(subItem => ContentfulHelpers.EntryIsNotALink(subItem.Sys)
+                                                      && _dateComparer.DateNowIsWithinSunriseAndSunsetDates(subItem.SunriseDate, subItem.SunsetDate))
+                    .Select(item => _subitemFactory.ToModel(item)).ToList();
 
             var featuredItems =
                 entry.FeaturedItems.Where(featItem => ContentfulHelpers.EntryIsNotALink(featItem.Sys)
-                && _dateComparer.DateNowIsWithinSunriseAndSunsetDates(featItem.SunriseDate, featItem.SunsetDate))
-                .Select(item => _subitemFactory.ToModel(item)).ToList();
-
-            var breadcrumbs =
-                entry.Breadcrumbs.Where(section => ContentfulHelpers.EntryIsNotALink(section.Sys))
-                                               .Select(crumb => _crumbFactory.ToModel(crumb)).ToList();
+                                                      && _dateComparer.DateNowIsWithinSunriseAndSunsetDates(featItem.SunriseDate, featItem.SunsetDate))
+                    .Select(item => _subitemFactory.ToModel(item)).ToList();
 
             var consultations =
                 entry.Consultations.Where(section => ContentfulHelpers.EntryIsNotALink(section.Sys))
-                                               .Select(consult => _consultationFactory.ToModel(consult)).ToList();
-
-            var socialMediaLinksSubheading = !string.IsNullOrEmpty(entry.SocialMediaLinksSubheading)
-                ? entry.SocialMediaLinksSubheading
-                : "";
+                    .Select(consult => _consultationFactory.ToModel(consult)).ToList();
 
             var socialMediaLinks = entry.SocialMediaLinks.Where(media => ContentfulHelpers.EntryIsNotALink(media.Sys))
-                                               .Select(media => _socialMediaFactory.ToModel(media)).ToList();
+                .Select(media => _socialMediaFactory.ToModel(media)).ToList();
 
-            var alerts = entry.Alerts.Where(alert => ContentfulHelpers.EntryIsNotALink(alert.Sys) &&
-                                           _dateComparer.DateNowIsWithinSunriseAndSunsetDates(alert.SunriseDate, alert.SunsetDate))
-                                           .Select(alert => _alertFactory.ToModel(alert)).ToList();
+            var breadcrumbs =
+                entry.Breadcrumbs.Where(section => ContentfulHelpers.EntryIsNotALink(section.Sys))
+                    .Select(crumb => _crumbFactory.ToModel(crumb)).ToList();
 
-            var keyFacts = entry.KeyFacts.Where(fact => ContentfulHelpers.EntryIsNotALink(fact.Sys))
-                                               .Select(fact => _keyFactFactory.ToModel(fact)).ToList();
-
-            var profile = entry.Profile == null ? null : _profileFactory.ToModel(entry.Profile);
+            var profile = entry.Profile != null
+                ? _profileFactory.ToModel(entry.Profile)
+                : null;
 
             var profiles = entry.Profiles.Where(singleProfile => ContentfulHelpers.EntryIsNotALink(singleProfile.Sys))
                 .Select(singleProfile => _profileFactory.ToModel(singleProfile)).ToList();
-            var callToActionBanner = entry.CallToActionBanner == null ? null : _callToActionBannerContentfulFactory.ToModel(entry.CallToActionBanner);
 
-            var keyFactSubheading = !string.IsNullOrEmpty(entry.KeyFactSubheading)
-                ? entry.KeyFactSubheading
-                : "";
+            var keyFacts = entry.KeyFacts.Where(fact => ContentfulHelpers.EntryIsNotALink(fact.Sys))
+                .Select(fact => _keyFactFactory.ToModel(fact)).ToList();
 
-            var triviaSubheading = !string.IsNullOrEmpty(entry.TriviaSubheading)
-                ? entry.TriviaSubheading
-                : "";
+            var alerts = entry.Alerts.Where(alert => ContentfulHelpers.EntryIsNotALink(alert.Sys) &&
+                                                     _dateComparer.DateNowIsWithinSunriseAndSunsetDates(alert.SunriseDate, alert.SunsetDate))
+                .Select(alert => _alertFactory.ToModel(alert)).ToList();
+
+            var tertiaryItems = entry.TertiaryItems.Where(subItem => ContentfulHelpers.EntryIsNotALink(subItem.Sys)
+                                                                     && _dateComparer.DateNowIsWithinSunriseAndSunsetDates(subItem.SunriseDate, subItem.SunsetDate))
+                .Select(subItem => _subitemFactory.ToModel(subItem)).ToList();
 
             var triviaSection = entry.TriviaSection.Where(fact => ContentfulHelpers.EntryIsNotALink(fact.Sys))
                 .Select(fact => _informationListFactory.ToModel(fact)).ToList();
 
-            var video = entry.Video == null ? null : _videoFactory.ToModel(entry.Video);
-           
-            return new Showcase(
-                slug,
-                title,
-                secondaryItems,
-                heroImage,
-                subHeading,
-                teaser,
-                breadcrumbs,
-                consultations,
-                socialMediaLinksSubheading,
-                socialMediaLinks,
-                eventSubheading,
-                eventCategory,
-                newsSubheading,
-                newsCategoryTag,
-                bodySubheading,
-                body,
-                emailAlertsTopicId,
-                emailAlertsText,
-                alerts,
-                primaryItems,
-                featuredItemSubheading,
-                featuredItems,
-                keyFacts,
-                profile,
-                profiles,
-                entry.FieldOrder,
-                keyFactSubheading,
-                entry.Icon,
-                subItems,
-                tertiaryItems,
-                triviaSubheading,
-                triviaSection,
-                callToActionBanner,
-                entry.ProfileHeading,
-                entry.ProfileLink,
-                entry.EventsReadMoreText,
-                video
-            ).StripData(_httpContextAccessor);
+            var callToActionBanner = entry.CallToActionBanner != null
+                ? _callToActionBannerContentfulFactory.ToModel(entry.CallToActionBanner)
+                : null;
+
+            var video = entry.Video != null
+                ? _videoFactory.ToModel(entry.Video)
+                : null;
+
+            return new Showcase
+            {
+                Title = entry.Title,
+                Slug = entry.Slug,
+                HeroImageUrl = heroImage,
+                PrimaryItems = primaryItems,
+                Teaser = entry.Teaser,
+                Subheading = entry.Subheading,
+                SecondaryItems = secondaryItems,
+                FeaturedItemsSubheading = entry.FeaturedItemsSubheading,
+                FeaturedItems = featuredItems,
+                Consultations = consultations,
+                SocialMediaLinksSubheading = entry.SocialMediaLinksSubheading,
+                SocialMediaLinks = socialMediaLinks,
+                EventSubheading = entry.EventSubheading,
+                EventCategory = entry.EventCategory,
+                EventsReadMoreText = entry.EventsReadMoreText,
+                NewsSubheading = entry.NewsSubheading,
+                NewsCategoryTag = entry.NewsCategoryTag,
+                Breadcrumbs = breadcrumbs,
+                BodySubheading = entry.BodySubheading,
+                Body = entry.Body,
+                Profile = profile,
+                ProfileHeading = entry.ProfileHeading,
+                ProfileLink = entry.ProfileLink,
+                Profiles = profiles,
+                FieldOrder = entry.FieldOrder,
+                EmailAlertsTopicId = entry.EmailAlertsTopicId,
+                EmailAlertsText = entry.EmailAlertsText,
+                KeyFactSubheading = entry.KeyFactSubheading,
+                KeyFacts = keyFacts,
+                Alerts = alerts,
+                Icon = entry.Icon,
+                TertiaryItems = tertiaryItems,
+                TriviaSubheading = entry.TriviaSubheading,
+                TriviaSection = triviaSection,
+                CallToActionBanner = callToActionBanner,
+                Video = video
+            }.StripData(_httpContextAccessor);
         }
     }
 }
--- src/StockportContentApi/ContentfulModels/ContentfulShowcase.cs
+++ src/StockportContentApi/ContentfulModels/ContentfulShowcase.cs
@@ -7,36 +7,35 @@ namespace StockportContentApi.ContentfulModels
 {
     public class ContentfulShowcase : ContentfulReference
     {
-        public List<ContentfulReference> SecondaryItems { get; set; } = new List<ContentfulReference>();
-        public Asset HeroImage { get; set; } = new Asset{File = new File {Url = string.Empty}, SystemProperties = new SystemProperties {Type = "Asset"}};
+        public Asset HeroImage { get; set; } = new Asset { File = new File { Url = string.Empty }, SystemProperties = new SystemProperties { Type = "Asset" } };
+        public List<ContentfulReference> PrimaryItems { get; set; } = new List<ContentfulReference>();
         public string Subheading { get; set; } = string.Empty;
-        public List<ContentfulReference> Breadcrumbs { get; set; } = new List<ContentfulReference>();
+        public string FeaturedItemsSubheading { get; set; } = string.Empty;
+        public List<ContentfulReference> FeaturedItems { get; set; } = new List<ContentfulReference>();
+        public List<ContentfulConsultation> Consultations { get; set; } = new List<ContentfulConsultation>();
+        public string SocialMediaLinksSubheading { get; set; } = string.Empty;
+        public List<ContentfulSocialMediaLink> SocialMediaLinks { get; set; } = new List<ContentfulSocialMediaLink>();
         public string EventSubheading { get; set; } = string.Empty;
         public string EventCategory { get; set; } = string.Empty;
+        public string EventsReadMoreText { get; set; } = string.Empty;
         public string NewsSubheading { get; set; } = string.Empty;
         public string NewsCategoryTag { get; set; } = string.Empty;
+        public List<ContentfulReference> Breadcrumbs { get; set; } = new List<ContentfulReference>();
         public string BodySubheading { get; set; } = string.Empty;
         public string Body { get; set; } = string.Empty;
-        public IEnumerable<ContentfulAlert> Alerts { get; set; } = new List<ContentfulAlert>();
-        public List<ContentfulConsultation> Consultations { get; set; } = new List<ContentfulConsultation>();
-        public string SocialMediaLinksSubheading { get; set; } = String.Empty;
-        public List<ContentfulSocialMediaLink> SocialMediaLinks { get; set; } = new List<ContentfulSocialMediaLink>();
+        public ContentfulProfile Profile { get; set; } = null;
+        public string ProfileHeading { get; set; } = string.Empty;
+        public string ProfileLink { get; set; } = string.Empty;
+        public List<ContentfulProfile> Profiles { get; set; } = new List<ContentfulProfile>();
+        public FieldOrder FieldOrder { get; set; } = new FieldOrder();
         public string EmailAlertsTopicId { get; set; } = string.Empty;
         public string EmailAlertsText { get; set; } = string.Empty;
         public string KeyFactSubheading { get; set; } = string.Empty;
         public List<ContentfulKeyFact> KeyFacts { get; set; } = new List<ContentfulKeyFact>();
-        public List<ContentfulReference> PrimaryItems { get; set; } = new List<ContentfulReference>();
-        public string FeaturedItemsSubheading { get; set; }
-        public List<ContentfulReference> FeaturedItems { get; set; } = new List<ContentfulReference>();
-        public ContentfulProfile Profile { get; set; } = null;
-        public List<ContentfulProfile> Profiles { get; set; } = new List<ContentfulProfile>();
-        public string ProfileHeading { get; set; } = string.Empty;
-        public string ProfileLink { get; set; } = string.Empty;
-        public ContentfulCallToActionBanner CallToActionBanner { get; set; } = null;
-        public FieldOrder FieldOrder { get; set; } = new FieldOrder();
-        public string TriviaSubheading { get; set; }
+        public IEnumerable<ContentfulAlert> Alerts { get; set; } = new List<ContentfulAlert>();
+        public string TriviaSubheading { get; set; } = string.Empty;
         public List<ContentfulInformationList> TriviaSection { get; set; } = new List<ContentfulInformationList>();
-        public string EventsReadMoreText { get; set; }
+        public ContentfulCallToActionBanner CallToActionBanner { get; set; } = null;
         public ContentfulVideo Video { get; set; }
     }
 }
