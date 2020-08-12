d53986582e067fe8897263f8f1cefd1382260d26
Elena Alexandra <elena.secara@stockport.gov.uk>
2020-08-12 15:24:31 +0100

    tests(ServicePayPayment): add unit tests

--- test/StockportContentApiTests/Unit/Builders/ContentfulServicePayPaymentBuilder.cs
+++ test/StockportContentApiTests/Unit/Builders/ContentfulServicePayPaymentBuilder.cs
@@ -3,7 +3,7 @@ using StockportContentApi.ContentfulModels;
 
 namespace StockportContentApiTests.Unit.Builders
 {
-    internal class ContentfulPaymentBuilder
+    internal class ContentfulServicePayPaymentBuilder
     {
         private string _title = "title";
         private string _slug = "slug";
@@ -11,17 +11,17 @@ namespace StockportContentApiTests.Unit.Builders
         private string _description = "description";
         private string _paymentDetailsText = "paymentDetailsText";
         private string _referenceLabel = "referenceLabel";
-        private string _parisReference = "parisReference";
-        private string _fund = "fund";
-        private string _glCodeCostCentreNumber = "glCodeCostCentreNumber";
         private string _metaDescription = "metaDescription";
         private List<ContentfulAlert> _alerts = new List<ContentfulAlert>();
         private List<ContentfulReference> _breadcrumbs = new List<ContentfulReference> {
             new ContentfulReferenceBuilder().Build() };
+        private string _paymentAmount = "23.00";
+        private string _accountReference = "accountReference";
+        private string _catalogueId = "catalogueId";
 
-        public ContentfulPayment Build()
+        public ContentfulServicePayPayment Build()
         {
-            return new ContentfulPayment
+            return new ContentfulServicePayPayment
             {
                 Title = _title,
                 Slug = _slug,
@@ -29,77 +29,78 @@ namespace StockportContentApiTests.Unit.Builders
                 Description = _description,
                 PaymentDetailsText = _paymentDetailsText,
                 ReferenceLabel = _referenceLabel,
-                ParisReference = _parisReference,
-                Fund = _fund,
-                GlCodeCostCentreNumber = _glCodeCostCentreNumber,
                 Breadcrumbs = _breadcrumbs,
                 MetaDescription = _metaDescription,
-                Alerts = _alerts
+                Alerts = _alerts,
+                PaymentAmount = _paymentAmount,
+                AccountReference = _accountReference,
+                CatalogueId = _catalogueId
             };
         }
 
-        public ContentfulPaymentBuilder Slug(string slug)
+        public ContentfulServicePayPaymentBuilder Slug(string slug)
         {
             _slug = slug;
             return this;
         }
 
-        public ContentfulPaymentBuilder Title(string title)
+        public ContentfulServicePayPaymentBuilder Title(string title)
         {
             _title = title;
             return this;
         }
 
-        public ContentfulPaymentBuilder Teaser(string teaser)
+        public ContentfulServicePayPaymentBuilder Teaser(string teaser)
         {
             _teaser = teaser;
             return this;
         }
 
-        public ContentfulPaymentBuilder Description(string description)
+        public ContentfulServicePayPaymentBuilder Description(string description)
         {
             _description = description;
             return this;
         }
 
-        public ContentfulPaymentBuilder PaymentDetailsText(string paymentDetailsText)
+        public ContentfulServicePayPaymentBuilder PaymentDetailsText(string paymentDetailsText)
         {
             _paymentDetailsText = paymentDetailsText;
             return this;
         }
 
-        public ContentfulPaymentBuilder ReferenceLabel(string referenceLabel)
+        public ContentfulServicePayPaymentBuilder ReferenceLabel(string referenceLabel)
         {
             _referenceLabel = referenceLabel;
             return this;
         }
 
-        public ContentfulPaymentBuilder ParisReference(string parisReference)
-        {
-            _parisReference = parisReference;
-            return this;
-        }
-
-        public ContentfulPaymentBuilder Fund(string fund)
-        {
-            _fund = fund;
-            return this;
-        }
-        public ContentfulPaymentBuilder GlCodeCostCentreNumber(string glCodeCostCentreNumber)
-        {
-            _glCodeCostCentreNumber = glCodeCostCentreNumber;
-            return this;
-        }
-
-        public ContentfulPaymentBuilder Breadcrumbs(List<ContentfulReference> breadcrumbs)
+        public ContentfulServicePayPaymentBuilder Breadcrumbs(List<ContentfulReference> breadcrumbs)
         {
             _breadcrumbs = breadcrumbs;
             return this;
         }
 
-        public ContentfulPaymentBuilder Alerts(List<ContentfulAlert> alerts)
+        public ContentfulServicePayPaymentBuilder Alerts(List<ContentfulAlert> alerts)
         {
             _alerts = alerts;
+            return this;
+        }
+
+        public ContentfulServicePayPaymentBuilder PaymentAmount(string paymentAmount)
+        {
+            _paymentAmount = paymentAmount;
+            return this;
+        }
+
+        public ContentfulServicePayPaymentBuilder AccountReference(string accountReference)
+        {
+            _paymentAmount = accountReference;
+            return this;
+        }
+
+        public ContentfulServicePayPaymentBuilder CatalogueId(string catalogueId)
+        {
+            _paymentAmount = catalogueId;
             return this;
         }
     }
