7b8382c349e022f4fb8c5170b1dbb4237d1de83e
stephen.lally <stephen.lally.smbc@gmail.com>
2021-05-26 16:37:45 +0100

    chore(startup): remove kestrel

--- src/StockportContentApi/Program.cs
+++ src/StockportContentApi/Program.cs
@@ -17,23 +17,19 @@ namespace StockportContentApi
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseUrls("http://0.0.0.0:5001")
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .ConfigureKestrel((context, options) =>
-                {
-                    // Set properties and call methods on options
-                })
-                .ConfigureAppConfiguration((hostContext, config) =>
-                {
-                    config.Sources.Clear();
-                    config.SetBasePath(Directory.GetCurrentDirectory() + "/app-config");
-                    config
-                        .AddJsonFile("appsettings.json")
-                        .AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json");
-                    var tempConfig = config.Build();
-                    config.AddJsonFile(
-                        $"{tempConfig["secrets-location"]}/appsettings.{hostContext.HostingEnvironment.EnvironmentName}.secrets.json");
-                });
+                    .UseIISIntegration()
+                    .UseStartup<Startup>()
+                    .ConfigureAppConfiguration((hostContext, config) =>
+                    {
+                        config.Sources.Clear();
+                        config.SetBasePath(Directory.GetCurrentDirectory() + "/app-config");
+                        config
+                            .AddJsonFile("appsettings.json")
+                            .AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json");
+                        var tempConfig = config.Build();
+                        config.AddJsonFile(
+                            $"{tempConfig["secrets-location"]}/appsettings.{hostContext.HostingEnvironment.EnvironmentName}.secrets.json");
+                    });
             });
                 
     }
