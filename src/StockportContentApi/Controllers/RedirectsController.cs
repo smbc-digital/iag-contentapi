8defc5e22501fd1e6ba42f1be2db1e8fa8bf37f1
Lydia Gray <lydia.gray@stockport.gov.uk>
2021-09-02 09:42:20 +0100

    feat(redirects): add test endpoint for Contenful webhook

--- src/StockportContentApi/Controllers/RedirectsController.cs
+++ src/StockportContentApi/Controllers/RedirectsController.cs
@@ -1,6 +1,8 @@
 ﻿using StockportContentApi.Repositories;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StockportContentApi.ContentfulModels;
 
 namespace StockportContentApi.Controllers
 {
@@ -9,12 +11,15 @@ namespace StockportContentApi.Controllers
     {     
         private readonly ResponseHandler _handler;
         private readonly RedirectsRepository _repository;
+        private readonly ILogger<RedirectsController> _logger;
 
         public RedirectsController(ResponseHandler handler,
-            RedirectsRepository repository)
+            RedirectsRepository repository,
+            ILogger<RedirectsController> logger)
         {
             _handler = handler;
             _repository = repository;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -23,6 +28,14 @@ namespace StockportContentApi.Controllers
         public async Task<IActionResult> GetRedirects(string businessId)
         {
             return await _handler.Get(() => _repository.GetRedirects());
+        }
+
+        [HttpPost]
+        [Route("update-redirects")]
+        public async Task<IActionResult> UpdateRedirects(ContentfulRedirect body)
+        {
+            _logger.LogWarning($"RedirectsController:: UpdateRedirects body received: {body}");
+            return Ok();
         }
     }
 }
