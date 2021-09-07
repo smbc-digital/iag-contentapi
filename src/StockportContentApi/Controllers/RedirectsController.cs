4d461cd305f581996da0507c9ace973aecc2e059
Lydia Gray <lydia.gray@stockport.gov.uk>
2021-09-07 11:45:37 +0100

    refactor(Redirects): change update endpoint to PATCH, add update to Swagger

--- src/StockportContentApi/Controllers/RedirectsController.cs
+++ src/StockportContentApi/Controllers/RedirectsController.cs
@@ -1,39 +1,28 @@
-﻿using StockportContentApi.Repositories;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using StockportContentApi.ContentfulModels;
-using StockportContentApi.ContentfulFactories;
-using StockportContentApi.Model;
+using StockportContentApi.Repositories;
 
 namespace StockportContentApi.Controllers
 {
-    [ApiExplorerSettings(IgnoreApi = true)]
     public class RedirectsController : Controller
     {     
         private readonly ResponseHandler _handler;
         private readonly RedirectsRepository _repository;
-        private readonly ILogger<RedirectsController> _logger;
-        private readonly IContentfulFactory<ContentfulRedirect, BusinessIdToRedirects> _contenfulFactory;
 
         public RedirectsController(ResponseHandler handler,
-            RedirectsRepository repository,
-            ILogger<RedirectsController> logger,
-            IContentfulFactory<ContentfulRedirect, BusinessIdToRedirects> contentfulFactory)
+            RedirectsRepository repository)
         {
             _handler = handler;
             _repository = repository;
-            _logger = logger;
-            _contenfulFactory = contentfulFactory;
         }
 
+        [ApiExplorerSettings(IgnoreApi = true)]
         [HttpGet]
         [Route("redirects")]
         [Route("v1/redirects")]
         public async Task<IActionResult> GetRedirects() => await _handler.Get(() => _repository.GetRedirects());
 
-        [HttpPost]
+        [HttpPatch]
         [Route("update-redirects")]
         public async Task<IActionResult> UpdateRedirects() => await _handler.Get(() => _repository.GetUpdatedRedirects());
     }
