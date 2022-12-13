e6594025907dff959135471443e839481f43b03b
Stu <h.s.j.wilkes@gmail.com>
2022-12-13 07:21:02 +0000

    feat(NET6): rm unnecessary do while loop

--- src/StockportContentApi/Repositories/PrivacyNoticeRepository.cs
+++ src/StockportContentApi/Repositories/PrivacyNoticeRepository.cs
@@ -4,9 +4,6 @@ using StockportContentApi.Config;
 using StockportContentApi.ContentfulFactories;
 using StockportContentApi.ContentfulModels;
 using StockportContentApi.Model;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace StockportContentApi.Repositories
 {
@@ -43,38 +40,18 @@ namespace StockportContentApi.Repositories
         {
             var builder = new QueryBuilder<ContentfulPrivacyNotice>().ContentTypeIs("privacyNotice").Include(6);
 
-            var entries = GetAllEntries<ContentfulPrivacyNotice>("privacyNotice", builder);
+            var entries = await GetAllEntries<ContentfulPrivacyNotice>("privacyNotice", builder);
 
             var convertedEntries = entries.Select(entry => _contentfulFactory.ToModel(entry)).ToList();
 
             return convertedEntries;
         }
 
-        private List<T> GetAllEntries<T>(string contentType, QueryBuilder<T> builder)
+        private async Task<IEnumerable<T>> GetAllEntries<T>(string contentType, QueryBuilder<T> builder)
         {
-            var result = new List<T>();
+            var entries = await _client.GetEntries(builder.Limit(50).Skip(0));
 
-            var builderString = builder.Limit(50).Skip(49).Build();
-            builderString = builderString.Replace("skip=49", "skip=xx");
-
-            var totalItems = 0;
-            var skip = 0;
-
-            do
-            {
-                builderString = builderString.Replace("skip=xx", $"skip={skip}");
-
-                var entries = _client.GetEntries<T>(builderString).Result;
-
-                builderString = builderString.Replace($"skip={skip}", "skip=xx");
-
-                result = result.Concat(entries.Items).ToList();
-
-                totalItems = entries.Total;
-                skip += 50;
-            } while (result.Count() < totalItems);
-
-            return result;
+            return entries.Items;
         }
 
     }
--- src/StockportContentApi/Repositories/_BaseRepository.cs
+++ src/StockportContentApi/Repositories/_BaseRepository.cs
@@ -1,50 +1,29 @@
-﻿using System;
-using Contentful.Core.Models;
+﻿using Contentful.Core.Models;
 using Contentful.Core.Search;
 using StockportContentApi.Utils;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 
 namespace StockportContentApi.Repositories
 {
     public abstract class BaseRepository
     {
-        public async Task<ContentfulCollection<T>> GetAllEntriesAsync<T>(Contentful.Core.IContentfulClient _client, QueryBuilder<T> builder, ILogger logger = null)
+        public async Task<ContentfulCollection<T>> GetAllEntriesAsync<T>(Contentful.Core.IContentfulClient _client, QueryBuilder<T> builder, Microsoft.Extensions.Logging.ILogger logger = null)
         {
-            var result = new ContentfulCollection<T>();
-            result.Items = new List<T>();
-
-            if (!BuilderHasProperty(builder, "limit")) {
+            if (!BuilderHasProperty(builder, "limit"))
+            {
                 builder.Limit(ContentfulQueryValues.LIMIT_MAX);
             }
 
-            builder.Skip(999);
-            var builderString = builder.Build();
-            builderString = builderString.Replace("skip=999", "skip=xxx");
-            var totalItems = 0;
-            var skip = 0;
-            do
+            builder.Skip(0);
+            ContentfulCollection<T> result;
+            try
             {
-                builderString = builderString.Replace("skip=xxx", $"skip={skip}");
-                var items = new ContentfulCollection<T>();
-
-                try
-                {
-                    items = await _client.GetEntries<T>(builderString);
-                }
-                catch (Exception ex)
-                {
-                    logger?.LogError(ex, $"Could not get Entries: {ex.Message}");
-                    throw;
-                }
-
-                builderString = builderString.Replace($"skip={skip}", "skip=xxx");
-                totalItems = items.Total;
-                result.Items = result.Items.Concat(items.Items);
-                skip += ContentfulQueryValues.LIMIT_MAX;
-            } while (result.Items.Count() < totalItems);
+                result = await _client.GetEntries(builder);
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex, $"Could not get Entries: {ex.Message}");
+                throw;
+            }
 
             return result;
         }
