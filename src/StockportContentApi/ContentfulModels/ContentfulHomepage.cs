165427dce94a7f9d06b55baf2da9c3dab044f113
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2023-10-16 15:05:08 +0100

    patch: add primary call to action to homepage

--- src/StockportContentApi/ContentfulModels/ContentfulHomepage.cs
+++ src/StockportContentApi/ContentfulModels/ContentfulHomepage.cs
@@ -20,5 +20,6 @@ public class ContentfulHomepage : IContentfulModel
     public string MetaDescription { get; set; } = string.Empty;
     public ContentfulCarouselContent CampaignBanner { get; set; } = new ContentfulCarouselContent();
     public ContentfulCallToActionBanner CallToAction { get; set; } = null;
+    public ContentfulCallToActionBanner CallToActionPrimary { get; set; } = null;
     public IEnumerable<ContentfulSpotlightOnBanner> SpotlightOnBanner { get; set; } = null;
 }
