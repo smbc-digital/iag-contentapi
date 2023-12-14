ddb55ec1ca5ecb04902a9a8ded1b8c841f3e5644
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2023-12-14 16:59:29 +0000

    patch: fix and add  tests for directory repository

--- test/StockportContentApiTests/Unit/Repositories/DirectoryRepositoryTests.cs
+++ test/StockportContentApiTests/Unit/Repositories/DirectoryRepositoryTests.cs
@@ -5,11 +5,11 @@ namespace StockportContentApiTests.Unit.Repositories;
 public class DirectoryRepositoryTests
 {
     private readonly DirectoryRepository _repository;
-    // private readonly Directory _directory;
-    
+    private readonly Directory _directory;
+    private readonly DirectoryEntry _directoryEntry;
     private readonly Mock<IContentfulClient> _contentfulClient;
-    private readonly Mock<DirectoryContentfulFactory> _mockDirectoryContentfulFactory;
-    private readonly Mock<DirectoryEntryContentfulFactory> _mockDirectoryEntryContentfulFactory;
+    private readonly Mock<IContentfulFactory<ContentfulDirectory, Directory>> _mockDirectoryContentfulFactory;
+    private readonly Mock<IContentfulFactory<ContentfulDirectoryEntry, DirectoryEntry>> _mockDirectoryEntryContentfulFactory;
 
     public DirectoryRepositoryTests()
     {
@@ -20,47 +20,154 @@ public class DirectoryRepositoryTests
             .Add("TEST_MANAGEMENT_KEY", "KEY")
             .Build();
 
-        // _directory = new Directory{
-        //     Alerts = null,
-        //     Slug = "directory-slug",
-        //     Body = "Directory body",
-        //     CallToAction = null,
-        //     BackgroundImage = null,
-        //     Teaser = "Directory teaser",
-        //     MetaDescription = "Directory MetaDescription",
-        //     ContentfulId = "XXX123456",
-        //     Title = "Directory title"
-        // };
+        _directory = new Directory{
+            Alerts = null,
+            Slug = "directory-slug",
+            Body = "Directory body",
+            CallToAction = null,
+            BackgroundImage = null,
+            Teaser = "Directory teaser",
+            MetaDescription = "Directory MetaDescription",
+            ContentfulId = "XXX123456",
+            Title = "Directory title"
+        };
+
+        _directoryEntry = new DirectoryEntry{
+            Alerts = null,
+            Slug = "directory-entry-slug",
+            Body = "Directory entry body",
+            Teaser = "Directory entry teaser",
+            MetaDescription = "Directory entry MetaDescription",
+            Title = "Directory entry title"
+        };
 
         Mock<IContentfulClientManager> contentfulClientManager = new();
         _contentfulClient = new Mock<IContentfulClient>();
         contentfulClientManager.Setup(o => o.GetClient(config)).Returns(_contentfulClient.Object);
 
-        _mockDirectoryContentfulFactory = new Mock<DirectoryContentfulFactory>();
-        _mockDirectoryEntryContentfulFactory = new Mock<DirectoryEntryContentfulFactory>());
+        _mockDirectoryContentfulFactory = new Mock<IContentfulFactory<ContentfulDirectory, Directory>>();
+        _mockDirectoryEntryContentfulFactory = new Mock<IContentfulFactory<ContentfulDirectoryEntry, DirectoryEntry>>();
 
         _repository  = new DirectoryRepository(config, contentfulClientManager.Object, _mockDirectoryContentfulFactory.Object, _mockDirectoryEntryContentfulFactory.Object );
     }
 
-
-    public void Get_WithSlug_Should_ReturnSuccess()
+    [Fact]
+    public async void Get_WithSlug_Should_ReturnSuccess()
     {
         const string slug = "a-slug";
-        var contentfulTopic = new ContentfulTopicBuilder().Slug(slug).Build();
-        ContentfulCollection<ContentfulTopic> collection = new()
+        var contentfulDirectory = new DirectoryBuilder().WithSlug(slug).Build();
+        ContentfulCollection<ContentfulDirectory> collection = new()
         {
-            Items = new List<ContentfulTopic> { contentfulTopic }
+            Items = new List<ContentfulDirectory> { contentfulDirectory }
         };
 
-        var builder = new QueryBuilder<ContentfulDirectory>().ContentTypeIs("directory").FieldEquals("fields.slug", "a-slug").Include(1);
-        _contentfulClient.Setup(_ => _.GetEntries(It.Is<QueryBuilder<ContentfulDirectory>>(q => q.Build() == builder.Build()),
+        _contentfulClient.Setup(_ => _.GetEntries(It.IsAny<QueryBuilder<ContentfulDirectory>>(),
             It.IsAny<CancellationToken>())).ReturnsAsync(collection);
 
-        var response = AsyncTestHelper.Resolve(_repository.Get("a-slug"));
+        _contentfulClient.Setup(_ => _.GetEntries(It.IsAny<QueryBuilder<ContentfulDirectoryEntry>>(),
+            It.IsAny<CancellationToken>())).ReturnsAsync(new ContentfulCollection<ContentfulDirectoryEntry> { Items = new List<ContentfulDirectoryEntry>()});
+
+        _mockDirectoryContentfulFactory.Setup(_ => _.ToModel(contentfulDirectory)).Returns(_directory);
+
+        var response = await _repository.Get(slug);
 
         // Arrange
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         Assert.Equal(response.Get<Directory>(), _directory);
+    }
+
+        [Fact]
+    public async void Get_WithSlug_Should_GetDirecotryEntries()
+    {
+        const string slug = "a-slug";
+        var contentfulDirectory = new DirectoryBuilder().WithSlug(slug).Build();
+        var contentfulDirectoryEntry = new DirectoryEntryBuilder().WithSlug(slug).Build();
+        ContentfulCollection<ContentfulDirectory> collection = new()
+        {
+            Items = new List<ContentfulDirectory> { contentfulDirectory }
+        };
+
+        ContentfulCollection<ContentfulDirectoryEntry> entryCollection = new()
+        {
+            Items = new List<ContentfulDirectoryEntry> { contentfulDirectoryEntry }
+        };
+        _contentfulClient.Setup(_ => _.GetEntries(It.IsAny<QueryBuilder<ContentfulDirectory>>(),
+            It.IsAny<CancellationToken>())).ReturnsAsync(collection);
+
+        _contentfulClient.Setup(_ => _.GetEntries(It.IsAny<QueryBuilder<ContentfulDirectoryEntry>>(),
+            It.IsAny<CancellationToken>())).ReturnsAsync(entryCollection);
+
+        _mockDirectoryContentfulFactory.Setup(_ => _.ToModel(contentfulDirectory)).Returns(_directory);
+
+        var response = await _repository.Get(slug);
+
+        // Arrange
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        _contentfulClient.Verify(_ => _.GetEntries(It.IsAny<QueryBuilder<ContentfulDirectoryEntry>>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async void Get_WithSlug_Should_Return_NotFound_If_DirectoryDoesNotExist()
+    {
+        const string slug = "a-slug";
+        var contentfulDirectory = new DirectoryBuilder().WithSlug(slug).Build();
+        ContentfulCollection<ContentfulDirectory> collection = new()
+        {
+            Items = new List<ContentfulDirectory>()
+        };
+
+        _contentfulClient.Setup(_ => _.GetEntries(It.IsAny<QueryBuilder<ContentfulDirectory>>(),
+            It.IsAny<CancellationToken>())).ReturnsAsync(collection);
+
+        _mockDirectoryContentfulFactory.Setup(_ => _.ToModel(contentfulDirectory)).Returns(_directory);
+
+        var response = await _repository.Get(slug);
+
+        // Arrange
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+        [Fact]
+    public async void GetEntry_WithSlug_Should_ReturnSuccess()
+    {
+        const string slug = "a-slug";
+        var contentfulDirectoryEntry = new DirectoryEntryBuilder().WithSlug(slug).Build();
+        ContentfulCollection<ContentfulDirectoryEntry> collection = new()
+        {
+            Items = new List<ContentfulDirectoryEntry> { contentfulDirectoryEntry }
+        };
+
+        _contentfulClient.Setup(_ => _.GetEntries(It.IsAny<QueryBuilder<ContentfulDirectoryEntry>>(),
+            It.IsAny<CancellationToken>())).ReturnsAsync(collection);
+
+        _mockDirectoryEntryContentfulFactory.Setup(_ => _.ToModel(contentfulDirectoryEntry)).Returns(_directoryEntry);
+
+        var response = await _repository.GetEntry(slug);
+
+        // Arrange
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(response.Get<DirectoryEntry>(), _directoryEntry);
+    }
+
+        [Fact]
+    public async void GetEntry_WithSlug_Should_Return_NotFound_If_DirectoryEntryDoesNotExist()
+    {
+        const string slug = "a-slug";
+        var contentfulDirectoryEntry = new DirectoryEntryBuilder().WithSlug(slug).Build();
+        ContentfulCollection<ContentfulDirectoryEntry> collection = new()
+        {
+            Items = new List<ContentfulDirectoryEntry>()
+        };
+
+        _contentfulClient.Setup(_ => _.GetEntries(It.IsAny<QueryBuilder<ContentfulDirectoryEntry>>(),
+            It.IsAny<CancellationToken>())).ReturnsAsync(collection);
+
+        _mockDirectoryEntryContentfulFactory.Setup(_ => _.ToModel(contentfulDirectoryEntry)).Returns(_directoryEntry);
+
+        var response = await _repository.GetEntry(slug);
+
+        // Arrange
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     }
 }
 
