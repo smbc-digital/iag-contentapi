18d7b0140d06fb351c1fd737a7f3d437e4ddde03
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2024-04-17 10:51:34 +0100

    patch: update fields

--- src/StockportContentApi/ContentfulModels/ContentfulReference.cs
+++ src/StockportContentApi/ContentfulModels/ContentfulReference.cs
@@ -11,6 +11,7 @@ public class ContentfulReference : IContentfulModel
     public string Teaser { get; set; } = string.Empty;
     public string MetaDescription { get; set; } = string.Empty;
     public string Icon { get; set; } = string.Empty;
+    public string ColourScheme { get; set; } = "Teal";
     public DateTime SunriseDate { get; set; } = DateTime.MinValue.ToUniversalTime();
     public DateTime SunsetDate { get; set; } = DateTime.MaxValue.ToUniversalTime();
     public bool Highlight { get; set; } = false;
