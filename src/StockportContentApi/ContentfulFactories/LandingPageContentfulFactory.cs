639c35acd06cdcb5b7e1b3f05808ae7338a9e2c6
Elena Secara <elena.secara@stockport.gov.uk>
2024-08-08 15:17:10 +0100

    test(LandingPageContentfulFactoryTests): add unit tests

--- src/StockportContentApi/ContentfulFactories/LandingPageContentfulFactory.cs
+++ src/StockportContentApi/ContentfulFactories/LandingPageContentfulFactory.cs
@@ -15,24 +15,25 @@ public class LandingPageContentfulFactory : IContentfulFactory<ContentfulLanding
         _alertFactory = alertFactory;
     }
 
-    public LandingPage ToModel(ContentfulLandingPage entry) => 
-        new()
+    public LandingPage ToModel(ContentfulLandingPage entry) => entry is null
+        ? null
+        : new()
         {
             Slug = entry.Slug,
             Title = entry.Title,
             Subtitle = entry.Subtitle,
             Breadcrumbs = entry.Breadcrumbs.Where(section => ContentfulHelpers.EntryIsNotALink(section.Sys))
-                                            .Select(crumb => _crumbFactory.ToModel(crumb)).ToList(),
+                                        .Select(crumb => _crumbFactory.ToModel(crumb)).ToList(),
             Alerts = entry.Alerts.Where(alert => ContentfulHelpers.EntryIsNotALink(alert.Sys) && _dateComparer.DateNowIsWithinSunriseAndSunsetDates(alert.SunriseDate, alert.SunsetDate))
-                                    .Where(alert => !alert.Severity.Equals("Condolence"))
-                                    .Select(alert => _alertFactory.ToModel(alert)).ToList(),
+                                .Where(alert => !alert.Severity.Equals("Condolence"))
+                                .Select(alert => _alertFactory.ToModel(alert)).ToList(),
             Teaser = entry.Teaser,
             MetaDescription = entry.MetaDescription,
             Image = entry.Image?.SystemProperties is not null && ContentfulHelpers.EntryIsNotALink(entry.Image.SystemProperties) ?
-                                entry.Image.File.Url : string.Empty,
+                            entry.Image.File.Url : string.Empty,
             HeaderType = entry.HeaderType,
             HeaderImage = entry.HeaderImage?.SystemProperties is not null && ContentfulHelpers.EntryIsNotALink(entry.HeaderImage.SystemProperties) ?
-                                    entry.HeaderImage.File.Url : string.Empty,
+                                entry.HeaderImage.File.Url : string.Empty,
             ContentBlocks = entry.ContentBlocks,
             Content = entry.Content
         };
