b8956636ab7cb7bdf0bc0a1d761a7ff60db54526
Lydia Gray <lydia.gray@stockport.gov.uk>
2024-10-08 07:10:34 +0100

    chore(Footer): add Footer Content

--- src/StockportContentApi/ContentfulModels/ContentfulFooter.cs
+++ src/StockportContentApi/ContentfulModels/ContentfulFooter.cs
@@ -8,4 +8,7 @@ public class ContentfulFooter : ContentfulReference
     public string CopyrightSection { get; set; } = string.Empty;
     public List<ContentfulReference> Links { get; set; } = new();
     public List<ContentfulSocialMediaLink> SocialMediaLinks { get; set; } = new();
+    public string FooterContent1 { get; set; } = string.Empty;
+    public string FooterContent2 { get; set; } = string.Empty;
+    public string FooterContent3 { get; set; } = string.Empty;
 }
--- src/StockportContentApi/Models/Footer.cs
+++ src/StockportContentApi/Models/Footer.cs
@@ -7,12 +7,24 @@ public class Footer
     public string Slug { get; }
     public IEnumerable<SubItem> Links { get; }
     public IEnumerable<SocialMediaLink> SocialMediaLinks { get; }
+    public string FooterContent1 { get; }
+    public string FooterContent2 { get; }
+    public string FooterContent3 { get; }
 
-    public Footer(string title, string slug, IEnumerable<SubItem> subItems, IEnumerable<SocialMediaLink> socialMediaLinks)
+    public Footer(string title, 
+        string slug, 
+        IEnumerable<SubItem> subItems, 
+        IEnumerable<SocialMediaLink> socialMediaLinks, 
+        string footerContent1, 
+        string footerContent2, 
+        string footerContent3)
     {
         Title = title;
         Slug = slug;
         Links = subItems;
         SocialMediaLinks = socialMediaLinks;
+        FooterContent1 = footerContent1;
+        FooterContent2 = footerContent2;
+        FooterContent3 = footerContent3;
     }
 }
