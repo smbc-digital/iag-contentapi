ba33d1a306e5227e76df74baa864115b4897b621
Elena Secara <elena.secara@stockport.gov.uk>
2024-10-22 13:32:50 +0100

    chore(tests): code cleanup

---
+++ test/StockportContentApiTests/Unit/ContentfulFactories/VideoContentfulFactoryTests.cs
@@ -0,0 +1,38 @@
+﻿namespace StockportContentApiTests.Unit.ContentfulFactories;
+
+public class VideoContentfulFactoryTests
+{
+    [Fact]
+    public void ToModel_ShouldCreateVideoFromAContentfulVideo()
+    {
+        // Arrange
+        ContentfulVideo contentfulReference = new ContentfulVideoBuilder().Build();
+
+        // Act
+        Video result = new VideoContentfulFactory().ToModel(contentfulReference);
+
+        // Assert
+        Assert.Equal(contentfulReference.Heading, result.Heading);
+        Assert.Equal(contentfulReference.Text, result.Text);
+        Assert.Equal(contentfulReference.VideoEmbedCode, result.VideoEmbedCode);
+    }
+
+    [Fact]
+    public void ToModel_ShouldMapEmptyValues()
+    {
+        // Arrange
+        ContentfulVideo contentfulReference = new ContentfulVideoBuilder()
+            .WithHeading(string.Empty)
+            .WithText(string.Empty)
+            .WithVideoEmbedCode(string.Empty)
+            .Build();
+
+        // Act
+        Video result = new VideoContentfulFactory().ToModel(contentfulReference);
+
+        // Assert
+        Assert.Empty(result.Heading);
+        Assert.Empty(result.Text);
+        Assert.Empty(result.VideoEmbedCode);
+    }
+}
