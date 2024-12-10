aab84a1733f73b5fcb50abbd40b5e29243121bd0
Elena Secara <elena.secara@stockport.gov.uk>
2024-12-10 13:26:56 +0000

    chore(Organisation): fix groups added to organisation

--- src/StockportContentApi/Repositories/OrganisationRepository.cs
+++ src/StockportContentApi/Repositories/OrganisationRepository.cs
@@ -4,19 +4,19 @@ public class OrganisationRepository
 {
     private readonly IContentfulFactory<ContentfulOrganisation, Organisation> _contentfulFactory;
     private readonly IContentfulClient _client;
-    private readonly IGroupRepository _groupRepository;
+    private readonly Func<ContentfulConfig, CacheKeyConfig, IGroupRepository> _groupRepository;
 
     public OrganisationRepository(ContentfulConfig config,
         IContentfulFactory<ContentfulOrganisation, Organisation> contentfulFactory,
         IContentfulClientManager contentfulClientManager,
-        IGroupRepository groupRepository)
+        Func<ContentfulConfig, CacheKeyConfig, IGroupRepository> groupRepository)
     {
         _contentfulFactory = contentfulFactory;
         _client = contentfulClientManager.GetClient(config);
         _groupRepository = groupRepository;
     }
 
-    public async Task<HttpResponse> GetOrganisation(string slug)
+    public async Task<HttpResponse> GetOrganisation(string slug, ContentfulConfig config, CacheKeyConfig cacheKeyConfig)
     {
         QueryBuilder<ContentfulOrganisation> builder = new QueryBuilder<ContentfulOrganisation>().ContentTypeIs("organisation").FieldEquals("fields.slug", slug);
         ContentfulCollection<ContentfulOrganisation> entries = await _client.GetEntries(builder);
@@ -26,8 +26,17 @@ public class OrganisationRepository
             return HttpResponse.Failure(HttpStatusCode.NotFound, "No Organisation found");
 
         Organisation organisation = _contentfulFactory.ToModel(entry);
-        organisation.Groups = await _groupRepository.GetLinkedGroupsByOrganisation(slug);
+
+        organisation.Groups = await GetGroup(slug, config, cacheKeyConfig);
 
         return HttpResponse.Successful(organisation);
+    }
+
+    private async Task<List<Group>> GetGroup(string groupSlug, ContentfulConfig config, CacheKeyConfig cacheKeyConfig)
+    {
+        IGroupRepository groupRepository = _groupRepository(config, cacheKeyConfig);
+        List<Group> groups = await groupRepository.GetLinkedGroupsByOrganisation(groupSlug);
+
+        return groups;
     }
 }
