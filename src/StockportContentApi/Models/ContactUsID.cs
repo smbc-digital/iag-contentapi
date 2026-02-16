d56d3aeb4da8d64a17a030b97ebd76cd6f8a843d
Elena Secara <52406019+elena-secara@users.noreply.github.com>
2026-02-16 12:00:35 +0000

    chore(ContactUsId): add bcc email address

--- src/StockportContentApi/Models/ContactUsID.cs
+++ src/StockportContentApi/Models/ContactUsID.cs
@@ -1,9 +1,10 @@
 ﻿namespace StockportContentApi.Models;
 
 [ExcludeFromCodeCoverage]
-public class ContactUsId(string name, string slug, string emailAddress)
+public class ContactUsId(string name, string slug, string emailAddress, string bccAddress)
 {
     public string Name { get; } = name;
     public string Slug { get; } = slug;
     public string EmailAddress { get; } = emailAddress;
+    public string BccAddress { get; } = bccAddress;
 }
