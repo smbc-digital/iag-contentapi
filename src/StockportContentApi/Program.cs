8dfdd02819f760f7ea87f586af0cd7e23314bb31
Michael Hayes <michael.hayes@stockport.gov.uk>
2026-08-17 13:54:18 +0100

    chore(net10): upgrade service

--- src/StockportContentApi/Program.cs
+++ src/StockportContentApi/Program.cs
@@ -1,4 +1,6 @@
-﻿try
+﻿using TimeProvider = StockportContentApi.Utils.TimeProvider;
+
+try
 {
     WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -34,9 +36,65 @@
         .ReadFrom.Configuration(context.Configuration)
         .WriteToElasticsearchAws(builder.Configuration));
 
-    Log.Logger.Information($"CONTENTAPI : CONFIGURE APPLICATION START");
-    Startup startup = new(builder.Configuration, builder.Environment, Log.Logger);
-    startup.ConfigureServices(builder.Services);
+    // Configure Services
+    string appEnvironment = builder.Environment.EnvironmentName;
+    string contentRootPath = builder.Environment.ContentRootPath;
+    bool useRedisSession = builder.Configuration["UseRedisSessions"].Equals("true", StringComparison.OrdinalIgnoreCase);
+    bool useLocalCache = builder.Configuration["UseLocalCache"].Equals("true", StringComparison.OrdinalIgnoreCase);
+
+    Log.Logger.Information(
+        $"CONTENTAPI: STARTUP : ConfigureServices : Env = {appEnvironment}, UseRedisSession = {useRedisSession}, UseLocalCache = {useLocalCache}, ContentRoot = {contentRootPath}");
+
+    builder.Services.AddControllers().AddNewtonsoftJson();
+    builder.Services.AddSingleton(new CurrentEnvironment(appEnvironment));
+    builder.Services.AddCache(useRedisSession, appEnvironment, builder.Configuration, Log.Logger, useLocalCache);
+    builder.Services.AddSingleton(new TwentyThreeConfig(builder.Configuration["TwentyThreeBaseUrl"]));
+
+    builder.Services.AddSingleton<IHttpClient>(p =>
+        new LoggingHttpClient(new HttpClient(new MsHttpClientWrapper(), p.GetService<ILogger<HttpClient>>()),
+            p.GetService<ILogger<LoggingHttpClient>>()));
+
+    builder.Services.AddTransient<IHealthcheckService>(p => new HealthcheckService($"{contentRootPath}/version.txt",
+        $"{contentRootPath}/sha.txt", new FileWrapper(), appEnvironment));
+
+    builder.Services.AddTransient<ResponseHandler>();
+    builder.Services.AddSingleton<ITimeProvider>(new TimeProvider());
+    builder.Services.AddSingleton(builder.Configuration);
+    builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+    builder.Services.AddSingleton(_ => new ShortUrlRedirects(new()));
+    builder.Services.AddSingleton(_ => new LegacyUrlRedirects(new()));
+
+    builder.Services.AddRedirects(builder.Configuration);
+    builder.Services.AddContentfulConfig(builder.Configuration);
+    builder.Services.AddCacheKeyConfig(builder.Configuration);
+    builder.Services.AddOptions();
+    builder.Services.AddContentfulClients();
+    builder.Services.AddContentfulFactories();
+    builder.Services.AddRepositories();
+    builder.Services.AddAutoMapper();
+    builder.Services.AddServices();
+    builder.Services.AddBuilders();
+
+    builder.Services.Configure<RedisExpiryConfiguration>(builder.Configuration.GetSection("redisExpiryTimes"));
+
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new() { Title = "Stockport Content API", Version = "v1" });
+        c.DocumentFilter<SwaggerFilter>();
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            In = ParameterLocation.Header,
+            Description = "Authorization using the Bearer scheme. Example: \"Authorization: Bearer {token}\""
+        });
+
+        c.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+        {
+            [new OpenApiSecuritySchemeReference("Bearer", document)] = []
+        });
+    });
 
     Log.Logger.Information($"CONTENTAPI : BUILDING APPLICATION");
     WebApplication app = builder.Build();
